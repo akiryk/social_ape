bf6f0d42bafb7130b2405b866b7442b4b0191398
Adam Kiryk <akiryk@wayfair.com>
2019-12-11 20:12:03 -0500

    Add some funcs

--- index.js
+++ index.js
@@ -36,3 +36,64 @@ app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('us-east1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region('us-east1')
+  .firestore.document('likes/{id}')
+  .onCreate(snapshot => {
+    db.doc(`screams/${snapshot.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.DeleteNotificationOnUnlike = functions
+  .region('us-east1')
+  .firestore.document('likes/{id}')
+  .onDelete(snapshot => {
+    db.doc(`notifications/${snapshot.id}`)
+      .delete()
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region('us-east1')
+  .firestore.document('comments/{id}')
+  .onCreate(snapshot => {
+    db.doc(`screams/${snapshot.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          });
+        }
+        return null;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
