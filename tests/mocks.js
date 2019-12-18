57cb5c88d2592dfcfa12c5ee4f4622de1610ef14
Adam Kiryk <akiryk@wayfair.com>
2019-12-17 22:04:06 -0500

    Add a test

---
+++ tests/mocks.js
@@ -0,0 +1,33 @@
+const mockScreams = {
+  orderBy: () => ({
+    limit: () => ({
+      get: () =>
+        Promise.resolve([
+          {
+            screamId: 'nuyrIJLAkh2TOdN47l9w',
+            data: () => ({
+              body: "Sometimes, you write things here, and I don't agree",
+              userHandle: 'JohnJohn',
+              likeCount: 2,
+              commentCount: 3,
+              createdAt: '2019-12-15T14:20:09.822Z',
+            }),
+          },
+          {
+            screamId: '9770ZONzTyau9e7zrKQx',
+            data: () => ({
+              body: 'Check it out, a live thing',
+              userHandle: 'akiryk',
+              likeCount: 2,
+              commentCount: 2,
+              createdAt: '2019-12-14T13:10:01.300Z',
+            }),
+          },
+        ]),
+    }),
+  }),
+};
+
+module.exports = {
+  mockScreams,
+};
---
+++ tests/screams.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const { getAllScreams } = require('../handlers/screams.js');
+const { db } = require('../util/admin');
+const { mockScreams } = require('./mocks');
+
+const { expect } = chai;
+
+describe('Screams', function() {
+  it('getAllScreams should return all screams', function() {
+    const req = {};
+    // Have `res` have a send key with a function value coz we use `res.send()` in our func
+    const res = {
+      json: sinon.spy(),
+    };
+
+    const dbStub = sinon.stub(db, 'collection').returns(mockScreams);
+
+    getAllScreams(req, res);
+
+    expect(dbStub.calledOnce).to.be.true;
+  });
+});
